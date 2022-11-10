7b7e7f100b38e1b2ee61674312af11296bf56b25
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2022-11-10 15:23:20 -0300

    Itens no Local Storage
    Os itens adicionados são salvos no local storage

--- js/main.js
+++ js/main.js
@@ -1,29 +1,44 @@
 const form = document.getElementById('novoItem')
 const lista = document.getElementById('lista')
+//Lista de itens que será salva no navagador.
+const itens = []
 
-form.addEventListener('submit', (evento)=>{
+form.addEventListener('submit', (evento) => {
     evento.preventDefault()
-    //Captura os campos "nome" e "quantidade"
-    const nome =  evento.target.elements['nome'].value
-    const quantidade =  evento.target.elements['quantidade'].value
-    
+
+    //Captura os campos "nome" e "quantidade".
+    const nome = evento.target.elements['nome'].value
+    const quantidade = evento.target.elements['quantidade'].value
+    //Cria elementos
     criaElemento(nome, quantidade)
+    //Limpa campos.
+    nome = ''
+    quantidade = ''
 
 })
-
-function criaElemento(nome,quantidade){
-    //Novo item criado
+//Função para criar elementos
+function criaElemento(nome, quantidade) {
+    //Novo item criado.
     const novoItem = document.createElement('li')
     //Classe adcionado
     novoItem.classList.add('item')
-    //Quantidade do item foi criado
+    //Quantidade do item foi criado.
     const numeroItem = document.createElement('strong')
     numeroItem.innerHTML = quantidade
-    //Incluir a quantidade do item no item
+    //Incluir a quantidade do item no item.
     novoItem.appendChild(numeroItem)
-    //Concatenar com o nome
+    //Concatenar com o nome.
     novoItem.innerHTML += nome
-    //Incluir o item na lista
+    //Incluir o item na lista.
     lista.appendChild(novoItem)
-    
+    //Item atual transformado em objeto.
+    const itemAtual = {
+        'nome': nome,
+        'quanidade': quantidade
+    }
+    //Esse objeto e colocado dentro de um array.
+    itens.push(itemAtual)
+    //Esse array de objetos e mandado para o local storage. Colocamos o JSON.stringify para transforma em string pois o localStorage so salva esse tipo.
+    localStorage.setItem('item', JSON.stringify(itens))
 }
+
